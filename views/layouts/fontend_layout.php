e0ca558a087f83d5167fbddfe4ae7560da2f0090
thanhdv <thanhdv193@gmail.com>
2016-02-15 10:42:29 +0700

    update new

--- views/layouts/fontend_layout.php
+++ views/layouts/fontend_layout.php
@@ -23,66 +23,9 @@ AppAsset::register($this);
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
-    <body class="home option3">
+    <body>
         <?php $this->beginBody() ?>
-        <!-- HEADER --> 
-        <?= HeaderWidget::widget() ?>
-        <!--END HEADER -->
-        <!-- Home slideder-->
-        <?= SlidederWidget::widget() ?>
-        <!--END Home slideder-->
-        <!-- servives -->
-        <div class="services-wapper">
-            <div class="container">
-                <div class="service ">
-                    <div class="col-xs-6 col-sm-3 service-item">
-                        <div class="icon">
-                            <img alt="services" src="/images/s1.png" />
-                        </div>
-                        <div class="info">
-                            <a href="#"><h3>Free Shipping</h3></a>
-                            <span>On order over $200</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 service-item">
-                        <div class="icon">
-                            <img alt="services" src="/images/s2.png" />
-                        </div>
-                        <div class="info">
-                            <a href="#"><h3>30-day return</h3></a>
-                            <span>Moneyback guarantee</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 service-item">
-                        <div class="icon">
-                            <img alt="services" src="/images/s3.png" />
-                        </div>
-
-                        <div class="info" >
-                            <a href="#"><h3>24/7 support</h3></a>
-                            <span>Online consultations</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 service-item">
-                        <div class="icon">
-                            <img alt="services" src="/images/s4.png" />
-                        </div>
-                        <div class="info">
-                            <a href="#"><h3>SAFE SHOPPING</h3></a>
-                            <span>Safe Shopping Guarantee</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end services -->
         
-        <?= $content ?>
-        
-        <!-- Footer -->
-        <?= FooterWidget::widget() ?>
-        <!--END Footer -->
-        <a href="#" class="scroll_top" title="Lên đầu trang" style="display: inline;">Scroll</a>
         <?php $this->endBody() ?>
     </body>
 </html>
