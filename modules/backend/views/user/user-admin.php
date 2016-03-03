3fa95d403e7d50914a146261d0e90de84ef1b71d
thanhdv193 <thanhdv1932gmail.com>
2016-03-03 21:48:14 +0700

    update

--- modules/backend/views/user/user-admin.php
+++ modules/backend/views/user/user-admin.php
@@ -10,21 +10,44 @@ use yii\helpers\Url;
 $this->title = 'Danh sách quản trị';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="panel">
-    <div class="panel-heading">
-            <h1><?= Html::encode($this->title) ?></h1>
-        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-        <p>           
-        </p>
+<div class="panel">        
+    <div class="panel-body">  
+        <div class="col-sm-5 col-md-12 col-lg-6">
+        <div class="panel panel-inverse">
+            <div class="panel-heading">
+                <h4 class="panel-title">Danh sách quản trị</h4>
+            </div>
+            <div class="panel-body">
+                <ul class="media-list user-list">
+                    <?php foreach ($listData as $value)
+                    { ?>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#">
+                                    <?php if($value['avatar'] != null){ ?>
+                                         <img class="media-object img-circle" src="<?php echo Url::base('http') ?>/<?php echo $value['avatar'] ?>" alt="">                                    
+                                    <?php }else { ?>
+                                        <img src="<?php echo Url::base('http') ?>/upload/avatar/no-avatar.png" alt="">
+                                    <?php }?>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading"><a href="#"><?php echo $value['username'] ?></a></h4>
+                                <span>5,323</span> Followers
+                            </div>
+                            <div class="media-right">
+                                <a href="<?php echo Url::base('http') ?>/backend/default/permission-user?id=<?php echo $value['id'] ?>">Cấp quyền</a>
+                            </div>
+                        </li>                                                 
+                    <?php } ?>
+                                       
+                </ul>
+            </div>
+        </div>
+    </div>
     </div>
     
-    <div class="panel-body">
-        <?php foreach($listData as $value){ ?>
-            <div><?php echo $value['username'] ?></div>
-            <div><a href="<?php echo Url::base('http') ?>/backend/default/permission-user?id=<?php echo $value['id'] ?>">Cấp quyền</a></div>
-        <?php }?>
-    </div>
+          </div>
     
 
 </div>
